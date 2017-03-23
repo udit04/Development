a5c8ea121e145d9a68dd80ba32af57532d9c7263
udit04 <udiwal789@gmail.com>
2017-03-23 17:20:34 +0530

    token >> removed

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 var script = {};
 script.hello = function (cb){
-<<<<<<< HEAD
 var result ="Hello World test106";
-=======
 var result ="Hello World test109";
->>>>>>> 83b72b307102459a1f323b753a66e1ff27a23ca9
 cb(result);
 }
 module.exports = script
--- testtwo.js
+++ testtwo.js
@@ -5,11 +5,8 @@ var expect = require('chai').expect,
 describe("hello sample test",function(){
 	it("must return Hello World", function(done) {
 		base_url.hello(function(reply){
-<<<<<<< HEAD
 			expect(reply).to.be.equal("Hello World test111");
-=======
 			expect(reply).to.be.equal("Hello World test109");
->>>>>>> 83b72b307102459a1f323b753a66e1ff27a23ca9
 			done();
 		});
     });
